218bd3991120fdb21f06cf10309b48781c16444e
Isaac <A212303A@correo.uia.mx>
2026-07-15 17:08:30 -0600

    Se agrega navegación automática a carrusel

--- src/app/carrusel/carrusel.ts
+++ src/app/carrusel/carrusel.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 
 @Component({
   selector: 'app-carrusel',
@@ -6,4 +6,23 @@ import { Component } from '@angular/core';
   templateUrl: './carrusel.html',
   styleUrl: './carrusel.css',
 })
-export class Carrusel {}
+export class Carrusel implements AfterViewInit {
+  ngAfterViewInit(): void {
+    let counter = 1;
+
+    setInterval(() => {
+      // Verificamos que estamos en navegador
+      if (typeof document !== 'undefined') {
+        const radio = document.getElementById('radio' + counter) as HTMLInputElement;
+        if (radio) {
+          radio.checked = true;
+        }
+      }
+
+      counter++;
+      if (counter > 3) {
+        counter = 1;
+      }
+    }, 5000);
+  }
+}
